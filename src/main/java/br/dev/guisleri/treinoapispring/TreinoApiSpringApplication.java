dae1421eab9ca24afd6d885e46d0369bb7405da9
marcosguisleri <marcos.guisleri.dev@gmail.com>
2026-04-17 16:01:20 -0300

    chore: initial project setup with Spring Initializr

---
+++ src/main/java/br/dev/guisleri/treinoapispring/TreinoApiSpringApplication.java
@@ -0,0 +1,13 @@
+package br.dev.guisleri.treinoapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/guisleri/treinoapispring/TreinoApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.guisleri.treinoapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
