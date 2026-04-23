1ead4ae9540840a3805b117abdcbd8d95a4c09cf
marcosguisleri <marcos.guisleri.dev@gmail.com>
2026-04-23 16:29:35 -0300

    feat(service):     - Implement AlunoService class with service methods

---
+++ src/main/java/br/dev/guisleri/treinoapispring/service/AlunoService.java
@@ -0,0 +1,54 @@
+package br.dev.guisleri.treinoapispring.service;
+
+import br.dev.guisleri.treinoapispring.model.Aluno;
+import br.dev.guisleri.treinoapispring.repo.AlunoRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AlunoService implements IAlunoService {
+
+    private final AlunoRepo alunoRepo;
+
+    @Override
+    public Aluno save(Aluno aluno) {
+        return alunoRepo.save(aluno);
+    }
+
+    @Override
+    public Aluno findById(Long id) {
+        return alunoRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Aluno not found! | ID: " + id));
+    }
+
+    @Override
+    public Aluno update(Long id, Aluno aluno) {
+        Aluno alunoExistente = alunoRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Aluno not found! | ID: " + id));
+
+        alunoExistente.setNome(aluno.getNome());
+        alunoExistente.setDataNascimento(aluno.getDataNascimento());
+        alunoExistente.setDataMatricula(aluno.getDataMatricula());
+        alunoExistente.setAtivo(aluno.isAtivo());
+
+        return alunoRepo.save(alunoExistente);
+    }
+
+    @Override
+    public List<Aluno> findAll() {
+        return alunoRepo.findAll();
+    }
+
+    @Override
+    public void delete(Long id) {
+        alunoRepo.deleteById(id);
+    }
+
+    @Override
+    public void deleteAll() {
+        alunoRepo.deleteAll();
+    }
+}
